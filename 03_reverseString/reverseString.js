d2d46f254f34ad48ae7ef5bff2f9eb1d0310a901
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-02-26 17:08:18 +0300

    done the third javascript exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(theString) {
+    let stringArray = theString.split("");
+    let reversedString = "";
+    for (let i = theString.length - 1; i > -1; i--) {
+        reversedString = reversedString + stringArray[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
