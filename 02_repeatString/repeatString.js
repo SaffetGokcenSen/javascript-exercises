640f21f7163b93733a2ce181b18360925dc8f4c4
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-02-26 16:55:54 +0300

    done second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(theString, repetitionNumber) {
+    let repeatedString = "";
+    if (repetitionNumber < 0) {
+        return "ERROR";
+    }
+    else {
+        for (let i = 0; i < repetitionNumber; ++i){
+            repeatedString = repeatedString  + theString;
+        }
+        return repeatedString;
+    }
 };
 
 // Do not edit below this line
