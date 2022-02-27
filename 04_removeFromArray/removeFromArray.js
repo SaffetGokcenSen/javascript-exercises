d1c636a372957273a9647561309c0e01be0f9c8f
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-02-27 14:47:17 +0300

    done the fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(theArray, ...theRest) {
+    let removalIndices = [];
+    let j = 0;
+    for (theArgument of theRest) {
+        for (let i = 0; i != theArray.length; ++i) {
+            if (theArgument === theArray[i]) {
+                removalIndices[j] = i;
+                ++j;
+            }
+        }
+    }
 
+    removalIndices.sort(function(a, b){return a-b});
+
+    j = 0;
+    for (theIndex of removalIndices) {
+        theArray.splice((theIndex-j), 1);
+        ++j;
+    }
+
+    return theArray;
 };
 
 // Do not edit below this line
