8fa318b4b7ae919293ee48c6ab1b2a884fc1bbb6
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-04-03 17:33:19 +0300

    done the calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function(addends) {
+  return addends.reduce((current, next) => current + next, 0) 
 };
 
-const multiply = function() {
-
+const multiply = function(multiplicands) {
+  if (multiplicands.length === 0) {
+    return null;
+  }
+  else {
+    return multiplicands.reduce((current, next) => current * next, 1);
+  }
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  if ((base === 0) && (power === 0)) {
+    return undefined;
+  }
+  else if (power === 0) {
+    return 1;
+  }
+  else {
+    let result = 1;
+    for (let i = 1; i <= power; ++i) {
+      result *= base;
+    }
+    return result;
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if ((n === 0) || (n === 1)) {
+    return 1;
+  }
+  else {
+    let result = 1;
+    for (let i = 1; i <= n; ++i) {
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
