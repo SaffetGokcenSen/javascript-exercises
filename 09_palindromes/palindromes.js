734d54db6925602c4bc8b2ba5b4b656b847827cb
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-04-05 00:27:21 +0300

    done the palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (theString) {
+    let punctuation = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', 
+    '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
+    '{', '|', '}', '~'];
 
+    punctuation.forEach((mark) => {
+        if (theString.includes(mark)) {
+            theString = theString.replaceAll(mark, '');
+        }
+    })
+
+    theString = theString.toLowerCase();
+    theString = theString.replaceAll(' ', '');
+    let theReverse = theString.split("").reverse().join("");
+    
+    return (theReverse === theString)
 };
 
 // Do not edit below this line
