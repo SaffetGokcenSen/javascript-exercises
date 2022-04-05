d200cf521cc781e075d1a53bc8e745cfc91c26b3
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-04-05 23:22:38 +0300

    written a function computing a specific member of the Fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let first = 1, second = 1;
+    if ((+n) <= 0) {
+        return("OOPS");
+    }
+    else if ((+n) === 1) {
+        return first;
+    }
+    else if ((+n) === 2) {
+        return second;
+    }
+    else {
+        let previous = first;
+        let current = second;
+        let next;
+        for (let i = 3; i <= (+n); ++i) {
+            next = current + previous;
+            previous = current;
+            current = next;
+        }
+        return next;
+    }
 };
 
 // Do not edit below this line
