e61d50c4e36caf9ea93ec6ce775e4afa453c267f
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-04-06 23:46:19 +0300

    done the findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.forEach((person) => {
+        if ("yearOfDeath" in person) {
+            person["age"] = person.yearOfDeath - person.yearOfBirth;
+        }
+        else {
+            let today = new Date();
+            let currentYear = today.getFullYear();
+            person["age"] = currentYear - person.yearOfBirth;
+        }
+    });
+    people.sort((person1, person2) => {
+        return person2["age"]-person1["age"]
+    });
 
+    return people[0];
 };
 
 // Do not edit below this line
